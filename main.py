934e0a2f9b8f498e4d421d5d4bfa9c9d0c627585
JAJA1706 <58956120+JAJA1706@users.noreply.github.com>
2020-04-06 00:53:01 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 
 app = FastAPI()
 app.counter = 0
+lista = []
 
 class request(BaseModel):
     name: str
@@ -16,7 +17,23 @@ class patient(BaseModel):
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
+
+@app.get("/patient/{pk}")#, response_model=what)
+async def patients(pk: int):
+    for lol in lista:
+        if pk in lista:
+            return(lista[lol])
+    return ("204")
+    
+    
+       
+        
+   # return patient(id=app.counter, patient = pt)
+
 @app.post("/patient", response_model=patient)
 async def patients(pt: request):
     app.counter += 1
+    lista.append(app.counter)
+    lista.append(pt)
     return patient(id=app.counter, patient = pt)
+
