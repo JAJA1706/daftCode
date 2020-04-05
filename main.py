375504d43d53b36943ee19b988417873d02bf426
JAJA1706 <58956120+JAJA1706@users.noreply.github.com>
2020-04-05 23:57:25 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+app.counter = 0
+
+class request(BaseModel):
+    name: str
+    surename: str
+    
+class patient(BaseModel):
+    id: int
+    rec: request
 
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
 
-@app.post("/patient", response=patient)
-def patient(rq: patient):
-    return patient(received=rq.dict())
+@app.get('/counter')
+def counter():
+    app.counter += 1
+    return str(app.counter)
 
+@app.post("/patient", response_model=patient)
+async def patients(rq: request):
+    app.counter += 1
+    return patient(id = app.counter, rec = rq)
