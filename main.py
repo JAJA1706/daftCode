4ebdcdf920db32690f094f1d757e37d672049ac4
JAJA1706 <58956120+JAJA1706@users.noreply.github.com>
2020-04-06 01:06:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from fastapi.responses import JSONResponse
 
 app = FastAPI()
 app.counter = 0
@@ -23,8 +24,19 @@ async def patients(pk: int):
     for lol in lista:
         if pk in lista:
             return(lista[lol])
-    return ("status_code=204")
+    return JSONResponse(status_code=204, content={})
     
+    
+       
+        
+   # return patient(id=app.counter, patient = pt)
+
+@app.post("/patient", response_model=patient)
+async def patients(pt: request):
+    app.counter += 1
+    lista.append(app.counter)
+    lista.append(pt)
+    return patient(id=app.counter, patient = pt)
     
        
         
