8513fa6d2de71d6acae9c251e6fc3672a6884d86
JAJA1706 <kuba1706@wp.pl>
2020-04-05 21:26:26 +0200

    dom3

--- main.py
+++ main.py
@@ -5,3 +5,8 @@ app = FastAPI()
 @app.get("/")
 def root():
     return {"message": "Hello World during the coronavirus pandemic!"}
+
+@app.post("/patient", response=patient)
+def patient(rq: patient):
+    return patient(received=rq.dict())
+
