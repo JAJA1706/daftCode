aeeed6410ea0ede4c010db89e4b6a619cb7c5cbb
JAJA1706 <kuba1706@wp.pl>
2020-05-02 02:48:12 +0200

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sqlite3
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
 app = FastAPI()
 
@@ -17,6 +18,14 @@ class Album(BaseModel):
     title: str
     artist_id: int
 
+class Customer(BaseModel):
+    Company: str = None
+    Address: str = None
+    City: str = None
+    State: str = None
+    Country: str = None
+    PostalCode: str = None
+    Fax: str = None
 
 @app.get("/")
 def root():
@@ -62,6 +71,58 @@ async def read_album( album_id: int ):
         "SELECT * FROM albums WHERE albumid = ?", (album_id,)).fetchall()
     return JSONResponse(status_code = 200, content={"AlbumId": album_id, "Title": album[0][1], "ArtistId": album[0][2]})
     
+    
+@app.put("/customers/{customer_id}")
+async def update_cust(customer_id: int, cust: Customer):
+    cursor = app.db_connection.cursor()
+    cursor.row_factory = sqlite3.Row
+    cust_select = cursor.execute(
+        "SELECT * FROM customers WHERE customerid = ?", (customer_id,)).fetchone()
+    if cust_select is None:
+        return JSONResponse(status_code = 404, content={"detail":{ "error": "Podales zle id customer"} })
         
-    
-    
+    #zamiana na dict
+    #dict_cust_select = dict(customerid=cust_select[0][0], firstname=cust_select[0][1], lastname=cust_select[0][2], company=cust_select[0][3], address=cust_select[0][4], city=cust_select[0][5], state=cust_select[0][6], country=cust_select[0][7], postalcode=cust_select[0][8], phone=cust_select[0][9], )
+    lista_key = []
+    lista_value = []
+    lista_end = []
+    znaleziony = False
+    dict_key_list = cust_select.keys()
+    update_cust = cust.dict(exclude_unset=True)
+    temp = "huk"
+    for x,y in update_cust.items():
+        lista_key.append(x)
+        lista_value.append(y)
+        
+    for i in range(13):
+        for x,y in cust:
+            if dict_key_list[i] == x:
+                for z in lista_key:
+                    if z == x:
+                        znaleziony = True
+                if znaleziony == True:
+                    lista_end.append(y)
+                    znaleziony = False
+                else:
+                    lista_end.append( cust_select[i] )
+            
+    cursor.execute(
+        "UPDATE customers SET company = ?, address = ?,  city = ?, state = ?, country = ?, postalcode = ?, fax = ?  WHERE customerid = ?", ( lista_end[0], lista_end[1], lista_end[2], lista_end[3], lista_end[4], lista_end[5], lista_end[6], customer_id,)
+    )
+    app.db_connection.commit()
+    customer = cursor.execute(
+        "SELECT * FROM customers WHERE customerid = ?", (customer_id,)).fetchall()
+    return JSONResponse(status_code = 200, content={
+        "CustomerId": customer[0][0],
+        "FirstName": customer[0][1],
+        "LastName": customer[0][2],
+        "Company": customer[0][3],
+        "Address": customer[0][4],
+        "City": customer[0][5],
+        "State": customer[0][6],
+        "Country": customer[0][7],
+        "PostalCode": customer[0][8],
+        "Phone": customer[0][9],
+        "Fax": customer[0][10],
+        "Email": customer[0][11],
+        "SupportRepId": customer[0][12]})
